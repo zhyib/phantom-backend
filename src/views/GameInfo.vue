cca6655abdfd68bc3be08a14a9af64117ecdb933
zhyib <zhyib002@outlook.com>
2023-03-09 17:06:54 +0800

    fix: type

--- src/views/GameInfo.vue
+++ src/views/GameInfo.vue
@@ -3,33 +3,63 @@
     <template #left>
       <el-form style="display: flex">
         <el-form-item label="Title">
-          <el-input v-model="pageData.searchTitle" @keyup.enter="handleClickSearch"></el-input>
+          <el-input
+            v-model="pageData.searchTitle"
+            @keyup.enter="handleClickSearch"
+          />
         </el-form-item>
-        <el-button @click="handleClickSearch">Search</el-button>
+        <el-button @click="handleClickSearch">
+          Search
+        </el-button>
       </el-form>
     </template>
     <template #right>
-      <el-button @click="handleClickAdd">Add</el-button>
+      <el-button @click="handleClickAdd">
+        Add
+      </el-button>
     </template>
   </TableTool>
   <el-table
-    :data="pageData.tableData">
-    <el-table-column prop="gameId" label="gameId" />
-    <el-table-column prop="titleEn" label="titleEn" />
-    <el-table-column prop="titleCn" label="titleCn" />
-    <el-table-column prop="rate" label="rate" />
-    <el-table-column prop="completed" label="completed">
+    :data="pageData.tableData"
+  >
+    <el-table-column
+      prop="gameId"
+      label="gameId"
+    />
+    <el-table-column
+      prop="titleEn"
+      label="titleEn"
+    />
+    <el-table-column
+      prop="titleCn"
+      label="titleCn"
+    />
+    <el-table-column
+      prop="rate"
+      label="rate"
+    />
+    <el-table-column
+      prop="completed"
+      label="completed"
+    >
       <template #default="scope">
         {{ isCompleted(scope.row.completed) }}
       </template>
     </el-table-column>
-    <el-table-column prop="isDeleted" label="isDeleted" />
-    <el-table-column prop="" label="Operation">
+    <el-table-column
+      prop="isDeleted"
+      label="isDeleted"
+    />
+    <el-table-column
+      prop=""
+      label="Operation"
+    >
       <template #default="scope">
         <el-button
           text
           bg
-          @click="handleClickEdit(scope.row.gameId)">
+          @click="handleClickEdit(scope.row.gameId)"
+        >
           Edit
         </el-button>
       </template>
@@ -37,9 +67,11 @@
   </el-table>
   <el-dialog
     v-model="pageData.dialogVisible"
-    title="编辑">
+    title="编辑"
+  >
     <el-form
-      label-width="auto">
+      label-width="auto"
+    >
       <el-form-item label="中文名">
         <el-input v-model="pageData.editData.titleCn" />
       </el-form-item>
@@ -65,10 +97,16 @@
         <el-input v-model="pageData.editData.publisher" />
       </el-form-item>
       <el-form-item label="短评">
-        <el-input type="textarea" v-model="pageData.editData.commentLine" />
+        <el-input
+          v-model="pageData.editData.commentLine"
+          type="textarea"
+        />
       </el-form-item>
       <el-form-item label="长评">
-        <el-input type="textarea" v-model="pageData.editData.commentText" />
+        <el-input
+          v-model="pageData.editData.commentText"
+          type="textarea"
+        />
       </el-form-item>
       <el-form-item label="游玩时间">
         <el-input-number v-model="pageData.editData.playedTime" />
@@ -76,24 +114,33 @@
       <el-form-item label="游玩日期">
         <el-date-picker
           v-model="pageData.editData.playDate"
-          type="date" />
+          type="date"
+        />
       </el-form-item>
       <el-form-item label="发行日期">
         <el-date-picker
           v-model="pageData.editData.publishDate"
-          type="date" />
+          type="date"
+        />
       </el-form-item>
       <el-form-item label="是否删除">
         <el-radio-group v-model="pageData.editData.isDeleted">
-          <el-radio :label="false">否</el-radio>
-          <el-radio :label="true">是</el-radio>
+          <el-radio :label="false">
+            否
+          </el-radio>
+          <el-radio :label="true">
+            是
+          </el-radio>
         </el-radio-group>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="pageData.dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="handleClickConfirm">
+        <el-button
+          type="primary"
+          @click="handleClickConfirm"
+        >
           Confirm
         </el-button>
       </span>
@@ -108,7 +155,6 @@ import axiosUtil from '@/utils/axiosUtil';
 
 import Game from '@/model/Game';
 import TableTool from '@/components/TaleTool.vue';
-import { fa } from 'element-plus/es/locale';
 
 interface GameTableData extends Game {
   completed?: string,
--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ module.exports = {
     // 设置代理
     proxy: {
       '/api': {
-        // target: 'http://1.12.243.205:3000/',
-        target: 'http://127.0.0.1:3000/',
+        target: 'http://1.12.243.205:3000/',
+        // target: 'http://127.0.0.1:3000/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
